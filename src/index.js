6c905c39127e1dde5cdca82253a1f17059fb00f4
Jason Park <jjpark987@gmail.com>
2022-11-15 13:24:40 -0800

    commit tree 0b60db488799aa9389bb3e1f5a4bc908138745bc

--- src/index.js
+++ src/index.js
@@ -17,62 +17,81 @@ document.addEventListener("DOMContentLoaded", () => {
   
   fetch('http://localhost:3000/toys')
   .then(response => response.json())
-  .then(toyData => addToysToDOM(toyData))
-  .catch(() => alert('There is an error with the fetch request.'))
-
-  function addToysToDOM(toyData) {
+  .then(toyData => {
     for(const toy of toyData) {
-      // Create a <div class="card"> for each toy
-      const toyDiv = document.createElement('div')
-      toyDiv.className = 'card'
-
-      // Add the name, image, number of likes and like button for each toy
-      const h2 = document.createElement('h2')
-      h2.textContent = toy.name
-      toyDiv.appendChild(h2)
-
-      const image = document.createElement('img')
-      image.src = toy.image
-      image.className = 'toy-avatar'
-      toyDiv.appendChild(image)
-
-      const p = document.createElement('p')
-      p.textContent = `This toy has ${toy.likes} likes!`
-      toyDiv.appendChild(p)
-
-      const button = document.createElement('button')
-      button.className = 'like-btn'
-      button.id = toy.id
-      button.textContent = ' Like ❤️ '
-      toyDiv.appendChild(button)
-
-      toyContainer.appendChild(toyDiv)
+      addToyToDOM(toy)
     }
+  })
+  .catch(() => alert('There was an error with the GET request.'))
+
+  function addToyToDOM(toy) {
+    // Create a <div class="card"> for each toy
+    const toyDiv = document.createElement('div')
+    toyDiv.className = 'card'
+
+    // Add the name, image, number of likes and like button for each toy
+    const h2 = document.createElement('h2')
+    h2.textContent = toy.name
+    toyDiv.appendChild(h2)
+
+    const image = document.createElement('img')
+    image.src = toy.image
+    image.className = 'toy-avatar'
+    toyDiv.appendChild(image)
+
+    const p = document.createElement('p')
+    p.textContent = `This toy has ${toy.likes} likes!`
+    toyDiv.appendChild(p)
+
+    const button = document.createElement('button')
+    button.className = 'like-btn'
+    button.id = toy.id
+    button.textContent = ' Like ❤️ '
+    // When the like button is clicked, edit the number of likes via PATCH
+    button.addEventListener('click', event => {
+      toy.likes ++
+      p.textContent = `This toy has ${toy.likes} likes!`
+
+      fetch(`http://localhost:3000/toys/${event.target.id}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+          'likes': toy.likes
+        })
+      })
+      .then(response => response.json())
+      .catch(error => alert('There was an error with the PATCH request.'))
+    })
+    toyDiv.appendChild(button)
+
+    toyContainer.appendChild(toyDiv)
   }
 
   // Add the user-submitted toy to the collection via POST
-  const userInputName = document.getElementsByName('name')
-  const userInputImage = document.getElementsByName('image')
-  const createButton = document.getElementsByName('submit')
+  const form = document.querySelector('.add-toy-form')
 
-  userInputName[0].addEventListener('submit', event => {
+  form.addEventListener('submit', event => {
     event.preventDefault()
-    console.log(event.target)
-  })
-  userInputImage[0].addEventListener('submit', event => {
-    event.preventDefault()
-    console.log(event.target)
-  })
-  createButton[0].addEventListener('submit', event => {
-    event.preventDefault()
-  })
+    
+    fetch('http://localhost:3000/toys', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        'name': event.target[0].value,
+        'image': event.target[1].value,
+        'likes': 0
+      })
+    })
+    .then(response => response.json())
+    .then(newToy => addToyToDOM(newToy))
+    .catch(error => alert('There was an error with the POST request.'))
 
-  // fetch('http://localhost:3000/toys', {
-  //   method: 'POST',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //     'Accept': 'application/json'
-  //   }
-  //   body: JSON.stringify()
-  // })
+    form.reset()
+  })
 });
